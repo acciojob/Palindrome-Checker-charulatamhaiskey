c7fa9601909f11e0365bc0b3d7e5720de4491494
Charulata Mhaiskey <109023984+charulatamhaiskey@users.noreply.github.com>
2023-03-06 21:08:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // complete the given function
 
 function palindrome(str){
-
+str = str.toLowerCase().replace(/(\W)|(\_)/g,"").split("");
+    for (var i = 0; i < str.length; i++) {
+        if (str[i] !== str[str.length - 1 -i]) {
+            return false;
+        }
+    }
+    return true;
 }
 module.exports = palindrome
